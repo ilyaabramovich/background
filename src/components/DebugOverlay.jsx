3592db1696b3aa7c47f5990dd0dc045a9093e49b
ilyaabramovich <ilyaabramovich1998@gmail.com>
2026-08-05 00:47:14 +0300

    Update DebugOverlay.jsx

--- src/components/DebugOverlay.jsx
+++ src/components/DebugOverlay.jsx
@@ -3,11 +3,14 @@ import { colorToIntArray } from "../utils";
 export default function DebugOverlay({ colors }) {
   return (
     <div
-      className="pointer-events-none fixed top-1 right-1 z-10 flex flex-col gap-2 text-xs"
+      className="pointer-events-none flex flex-col gap-2 text-xs"
       style={{
+        position: "fixed",
+        top: "1rem",
+        right: "1rem",
+        zIndex: 10,
         fontFamily: "ui-monospace, monospace",
         whiteSpace: "pre",
-        color: "#444",
       }}
     >
       {colors.map((color) => (
